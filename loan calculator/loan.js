a43e98cf45c2e435c0d1a5a9343ca20c95c4c5fc
Omkawalkar <95124394+Omkawalkar@users.noreply.github.com>
2026-06-09 13:36:03 +0530

    Add files via upload

---
+++ loan calculator/loan.js
@@ -0,0 +1,26 @@
+const loanAmount = document.getElementById("amount");
+const loanIntrest = document.getElementById("intrest");
+const loanmonth = document.getElementById("month");
+const button = document.getElementById("calculateButton");
+const resultValue = document.getElementById("result");
+
+const calculate = () => {
+
+    const amt = parseFloat(loanAmount.value);
+    const intr = parseFloat(loanIntrest.value);
+    const month = parseFloat(loanmonth.value);
+
+    if (isNaN(amt) || isNaN(intr) || isNaN(month)) {
+        resultValue.textContent = "Enter Valid Information";
+        return;
+    }
+
+    
+   const interest = (amt * intr * month) / (100 * 12);
+const total = amt + interest;
+const monthlyPayment = total / month;
+
+resultValue.textContent = monthlyPayment.toFixed(2);
+};
+
+button.addEventListener("click", calculate);
